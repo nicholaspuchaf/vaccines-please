eb63ffe61a899aff21190c059afdcfce1e4c569a
Nicholas <nicholas.pucharelli@gmail.com>
2025-06-11 09:58:44 -0300

    Criada a classe GenericFrame

--- Game/main.py
+++ Game/main.py
@@ -25,12 +25,125 @@ storyFrame1 = None
 storyFrame2 = None
 storyFrameUsing = 1
 root_window = None # Global reference to the root Tkinter window
+runningFrame = None
 
 # Variable of the story
 storyFrames = [("static/Mexicolandia.png","Essa é a Mexicolândia, um país assolado por miséria e doença. Eles ficaram nessa condição pois seu presidente não acreditou nas vacinas."), 
 ("static/EstadosVacinados1.png","Esse é os Estados Vacinados da América, um ótimo país saudável e muito avançado")]
 
 storyLabels = []
+
+class GenericFrame:
+
+    def __init__(self):
+        self.frame = None # Frame de Tkinter que esse Obj tera
+        self.texts = [] # colecao de texts para mostrar por esse obj
+        self.images = [] # colecao de images p mostrar
+        self.whichShow = 0 # indica qual foto e text mostrar
+        self.label = None # Ponteiro para o label onde esta a imagem
+        self.text_label = None # Ponteiro para onde esta guardado o texto
+        self.actual_text = None
+
+        self.next_button = None
+        self.previuous_button = None
+
+        self.ready = False
+
+    def isReady(self):
+        return self.ready
+
+    def add_text_and_image(self, text, image):
+        self.texts.append(text)
+        self.images.append(image)
+
+    def forget(self):
+        self.frame.forget()
+        
+    def pack_frame(self):
+        if self.ready == True:
+            self.frame.pack(pady=10, expand=True, fill="both")
+            self.show_story_text(0)
+        else:
+            print("Tried to pack a not ready frame")
+
+    def create_frame_with_image(self,parent,x=WIDTH/6,y=HEIGHT-60):
+        self.frame = tk.Frame(parent, bg=BG_COLOR, padx=1, pady=1)
+        image = None
+        try:
+            image = Image.open(self.images[self.whichShow])
+        except FileNotFoundError:
+            messagebox.showerror(f"Officer file not found at {self.images[self.whichShow]}")
+            return None    
+        except Exception as e:
+            messagebox.showerror(f"Error at Officer loading : {e}")
+            return None
+
+        image = image.convert("RGBA")
+        image = image.resize((WIDTH, HEIGHT), Image.LANCZOS)
+
+        tk_image = ImageTk.PhotoImage(image)
+
+        self.label = tk.Label(self.frame, image=tk_image)
+
+        self.label.image = tk_image
+        self.label.place(x=10,y=10,width=WIDTH-20, height=HEIGHT-20)
+        self.label.lower()
+
+        self.text_label = tk.Label(self.frame, bg=COLOR_DARK_CHARCOAL,
+                                   relief="sunken", borderwidth=2, anchor="nw",
+                                   font=(FONT_FAMILY, 12, "bold"),
+                                   fg="white",
+                                   wraplength=int(WIDTH*2/3)-10,
+                                   padx=5)
+
+        self.text_label.place(x=x, y=y, width=int(WIDTH*2/3), height=TALKING_HEIGHT)
+        # self.text_label.config(text=self.texts[self.whichShow])
+        self.actual_text = self.texts[self.whichShow]
+        self.ready = True
+
+        self.next_page_button()
+
+    def show_story_text(self, index):
+        if(index == 0):
+            self.text_label.config(text="")
+        self.text_label.config(text=self.actual_text[:index])
+        if index < len(self.actual_text):
+            self.text_label.after(TEXT_DELAY, self.show_story_text, index+1)
+
+    def next_page(self, add):
+        if self.whichShow + add < len(self.images) and self.whichShow >= 0:
+            self.whichShow = self.whichShow + add
+            image = None
+            try:
+                image = Image.open(self.images[self.whichShow])
+            except FileNotFoundError:
+                messagebox.showerror(f"Officer file not found at {self.images[self.whichShow]}")
+                return None    
+            except Exception as e:
+                messagebox.showerror(f"Error at Officer loading : {e}")
+                return None
+            image = image.convert("RGBA")
+            image = image.resize((WIDTH, HEIGHT), Image.LANCZOS)
+            tk_image = ImageTk.PhotoImage(image)
+            self.label.config(image=tk_image)
+            self.label.image = tk_image
+            self.actual_text = self.texts[self.whichShow]
+            self.show_story_text(0)
+        
+        if self.whichShow + add == len(self.images):
+            # start game
+            pass
+
+    def next_page_button(self):
+        self.next_button = tk.Button(self.frame, text="Proximo", bg=COLOR_DARK_CHARCOAL,
+                                    relief="sunken", borderwidth=2, anchor="nw",
+                                    font=(FONT_FAMILY, 12, "bold"),
+                                    fg="white",
+                                    command=lambda:self.next_page(1))
+        self.next_button.place(x=10, y=HEIGHT-60)
+    
+    def previous_page_button(self):
+        pass
 
 def officer_change_text(text, index):
     global talking_label
@@ -290,12 +403,14 @@ def generic_story_frame(imagePath, text):
     return text_label
 
 
-def test_frame():
+def test_frame(frameObj):
     global mainMenuFrame, storyFrame1, storyFrame2, storyFrameUsing, storyFrames, storyLabels
 
     if mainMenuFrame:
         mainMenuFrame.forget()
 
+    frameObj.pack_frame()
+    return
     thisFrame = None
 
     if storyFrameUsing == 1:
@@ -311,16 +426,25 @@ def test_frame():
         thisFrame.pack(pady=10, expand=True, fill="both")
         
 
+def handle_frames(frameObj):
+    global runningFrame
+
+    if frameObj.isReady == False:
+        return
+
+    if runningFrame != None:
+        runningFrame.forget()
+    frameObj.pack_frame()
+
+
 def create_window():
     """
     Creates a Tkinter window with a themed menu.
     """
-    global video_label, video_thread, stop_event, mainMenuFrame, startGameFrame, root_window, storyFrames
+    global runningFrame, video_label, video_thread, stop_event, mainMenuFrame, startGameFrame, root_window, storyFrames
 
     # Create the main window (root window)
     root_window = tk.Tk()
-
-
     # --- Theme Configuration (Buckshot Roulette inspired) ---
     # Dark background colors
     bg_color = COLOR_DARK_CHARCOAL # Dark charcoal
@@ -347,15 +471,19 @@ def create_window():
                        font=(font_family, font_size))
     root_window.config(menu=menu_bar) # Attach the menu bar to the root_window window
 
-    menu_bar.add_command(label="Start", command=start_game)
+    startFrame = GenericFrame()
+    startFrame.add_text_and_image(storyFrames[0][1], storyFrames[0][0])
+    startFrame.create_frame_with_image(root_window)
+
+    menu_bar.add_command(label="Start", command=lambda:handle_frames(startFrame))
     menu_bar.add_command(label="Options", command=show_options)
     menu_bar.add_command(label="Quit", command=lambda:on_closing(root_window))
-    menu_bar.add_command(label="Credits", command=test_frame)
+    menu_bar.add_command(label="Credits", command=lambda:test_frame())
 
     # --- Load First Story Frame ---
 
-    story_text_label = generic_story_frame(storyFrames[0][0], storyFrames[0][1])
-    storyLabels.append(story_text_label)
+    # story_text_label = generic_story_frame(storyFrames[0][0], storyFrames[0][1])
+    # storyLabels.append(story_text_label)
 
 
     # --- Start Game Frame ---
@@ -368,7 +496,7 @@ def create_window():
     # Using a Frame to better control padding and background for the label
     mainMenuFrame = tk.Frame(root_window, bg=bg_color, padx=20, pady=20)
     mainMenuFrame.pack(expand=True, fill="both") # Expand to fill available space
-
+    runningFrame = mainMenuFrame
     
     main_label = tk.Label(mainMenuFrame,
                           text="Vaccines, Please",
@@ -389,6 +517,6 @@ def create_window():
 
 
 if __name__ == "__main__":
+    
     root_window = create_window()
-
     root_window.mainloop()
