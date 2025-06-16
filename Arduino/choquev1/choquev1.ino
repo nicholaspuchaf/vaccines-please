8d0d38af24df0e2f4072b1ede1d901ad49c53030
Bruno <alfaomegarubi@gmail.com>
2025-06-16 17:33:13 -0300

    Adicionei mais segurança no sistema de choques(motivo: eu tomei um choque bem forte sem querer)

--- Arduino/choquev1/choquev1.ino
+++ Arduino/choquev1/choquev1.ino
@@ -1,14 +1,19 @@
 void setup() {
   pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH); 
   Serial.begin(9600);
 }
 
 void loop() {
   int safety = 0;
+  digitalWrite(8, HIGH);
   if (Serial.available()) {
     char c = Serial.read();
     if (c == 's') {
+      digitalWrite(8, LOW);
+      delay(500);
       digitalWrite(8, HIGH);
+      delay(1000);
       safety++;
     } 
   }
--- Game/Pyshock.py
+++ Game/Pyshock.py
@@ -10,35 +10,34 @@ def find_arduino_port():
     return None
 
 port = find_arduino_port()
+
 if port is not None:
     ser = serial.Serial(port=port, baudrate=9600, timeout=1)
+    ser.write(b'0')
 else:
     raise Exception("No Arduino found")
 
 def shock():
     ser.write(b's')
-    time.sleep(1)
-    #time.sleep(1)
-    ser.write(b'0')
 
-def main():
-    if not port:
-        print("❌ Arduino not found. Make sure it's connected.")
-        return
-
-    print(f"✅ Arduino found on {port}")
-
-    time.sleep(2)  # Wait for Arduino to reset
-
-    print("🔁 Turning LED ON")
-    ser.write(b's')
-    time.sleep(10)
-
-    print("🔁 Turning LED OFF")
-    ser.write(b'0')
-
-    ser.close()
-    print("✅ Done.")
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     if not port:
+#         print("❌ Arduino not found. Make sure it's connected.")
+#         return
+#
+#     print(f"✅ Arduino found on {port}")
+#
+#     time.sleep(2)  # Wait for Arduino to reset
+#
+#     print("🔁 Turning LED ON")
+#     ser.write(b's')
+#     time.sleep(10)
+#
+#     print("🔁 Turning LED OFF")
+#     ser.write(b'0')
+#
+#     ser.close()
+#     print("✅ Done.")
+#
+# if __name__ == "__main__":
+#     main()
